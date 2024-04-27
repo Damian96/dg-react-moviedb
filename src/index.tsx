d4880ab4d66e80266c43e2cd4ed016729b87a096
Damianos Giankakis <damgiankakis@gmail.com>
2024-04-27 21:15:44 +0300

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import configurestore from "./redux/configurestore";
+import {Provider} from "react-redux";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={configurestore}>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
